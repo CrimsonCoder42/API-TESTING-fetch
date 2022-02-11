4885c60eae9fbf6b307eea3ed57a132fbc65dc0d
Nostro37 <nostro37@gmail.com>
2022-02-11 14:25:54 -0500

    destructuring data from API complete

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("hello")
+let nameArray = [];
+
+const fetchPromise = fetch("https://swapi.dev/api/people/");
+fetchPromise.then(response => {
+  return response.json();
+}).then(people => {
+  let charData = people.results.forEach(character => {
+     const  {name, height, mass} = character
+    console.log(name)
+    console.log(height);
+    console.log(mass)
+  });
+  //let charNames = character.name;
+
+  //for (let i = 0; charData.length)
+});
+
