051d3946e72a34bb03cca4dd93058b95587f3a04
Nostro37 <nostro37@gmail.com>
2022-02-11 15:02:24 -0500

    updated API data

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-let nameArray = [];
-
-const fetchPromise = fetch("https://swapi.dev/api/people/");
+const fetchPromise = fetch("https://ghibliapi.herokuapp.com/people");
 fetchPromise.then(response => {
   return response.json();
 }).then(people => {
-  let charData = people.results.forEach(character => {
-     const  {name, height, mass} = character
-    console.log(name)
-    console.log(height);
-    console.log(mass)
-  });
-  //let charNames = character.name;
-
-  //for (let i = 0; charData.length)
+  const names = people.map(person => person.name).join("\n");
+  console.log(names);
 });
-
