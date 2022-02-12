370fc0b48f30076c978d94269be0e58a75c5a469
Nostro37 <nostro37@gmail.com>
2022-02-12 09:23:46 -0500

    back end logic finshed

--- app.js
+++ app.js
@@ -32,38 +32,3 @@ fetchPromise.then(response => {
          container.innerHTML += content;
        })
     });
-    
-
-
-
-// let namePost = (element) => {
-//     const {name, height, mass} = element;
-//     const newDiv = document.createElement("div");
-//     const content = `
-//     <div class="card">
-//     <div class="card-header" id="heading-${name}">
-//       <h5 class="mb-0">
-//         <button class="btn btn-link" data-toggle="collapse" data-target="#collapse-${name}" aria-expanded="true" aria-controls="collapse-${name}">
-
-//                 </button>
-//       </h5>
-//     </div>
-
-//     <div id="collapse-${name}" class="collapse show" aria-labelledby="heading-${name}" data-parent="#accordion">
-//       <div class="card-body">
-
-//         <h5>${name}</h5>
-//         <p>${height}</p>
-//         <p>${mass}</p>
-//         ...
-//       </div>
-//     </div>
-//   </div>
-//   `;
-
-//   newDiv.innerHTML(content);
-  
-//   // add the newly created element and its content into the DOM
-//   const currentDiv = document.getElementById("main");
-//   document.body.insertBefore(newDiv, currentDiv); 
-// }
